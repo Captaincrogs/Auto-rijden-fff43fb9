7148b834fc0a375aa60f874626c567067d41d1c8
Captaincrogs <2003829@talnet.nl>
2019-10-30 10:28:37 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+
+<?php
+
+$vraag = readline("Hoe oud ben je?: ");
+
+    if ($vraag <=  15){
+        echo "Helaas, je mag nog niet beginnen met rijlessen ";
+       
+	
+    }else 
+	echo "Je mag beginnen met rijlessen! ";
+
+?>
