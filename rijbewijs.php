6b2f126d8617d16a79d63c7a8fa424064dc6cbdc
Captaincrogs <54841728+Captaincrogs@users.noreply.github.com>
2020-07-10 17:37:57 +0200

    Add files via upload

--- rijbewijs.php
+++ rijbewijs.php
@@ -1,14 +1,12 @@
-#!/usr/bin/env php
-
 <?php
 
-$vraag = readline("Hoe oud ben je?: ");
+$leeftijd = readline("wat is je leeftijd ?");
+$a = (int) 100;
+$b = (float) 16.5;
 
-    if ($vraag <=  15){
-        echo "Helaas, je mag nog niet beginnen met rijlessen ";
-       
-	
-    }else 
-	echo "Je mag beginnen met rijlessen! ";
+if ($leeftijd > $b) {
+    echo "je mag gaan lessen";
 
-?>
+} else {
+    echo "je mag nog niet gaan lessen ";
+}
